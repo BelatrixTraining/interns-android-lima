3466bf22563eaa06d8ced3da094161524c81e76c
Eduardo Medina <emedinaa@gmail.com>
2018-12-08 07:14:16 -0500

    Added samples

---
+++ FragmentSamples/app/src/main/java/com/emedinaa/myfirstapp/ui/fragments/BlankFragmentListener.java
@@ -0,0 +1,17 @@
+package com.emedinaa.myfirstapp.ui.fragments;
+
+/**
+ * Created by emedinaa on 31/08/17.
+ */
+
+public interface BlankFragmentListener {
+
+    void actionFragment(Object object);
+    void actionActivityCambiarColor();
+
+    void callToActivity(Object object);
+
+    void cerrarSesion();
+
+    void loQueYoQuiera();
+}
